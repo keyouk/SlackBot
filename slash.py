b6932693974a8c2b6257f2c4de870f84a35e4788
Kelvin Youk <kelvin@kelvin-youk.attlocal.net>
2018-08-31 00:02:06 -0700

    Initialization

---
+++ slash.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from flask import Flask, request, jsonify, abort
+
+
+app = Flask(__name__)
+webhook = "insert webhook here"
+
+
+@app.route('/hello', methods=['GET','POST'])
+def hello():
+	if request.method == 'POST':      
+		data = request.form.get()     
+		
+		print data
+
+
+
+if __name__ == '__main__':
+	app.debug = True
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
